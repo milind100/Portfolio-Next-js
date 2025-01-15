3dc558c933c8672e3655ee8e214069ea35d7f986
milind <milind10rpatil@gmail.com>
2025-01-15 18:21:42 +0530

    social links addes

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,27 +1,27 @@
 export const socialLinks = [
   {
-    src: "/img/Facebook.png",
-    link: "www.google.com",
-    alt: "Facebook Logo",
-  },
-  {
-    src: "/img/Insta.png",
-    link: "www.google.com",
-    alt: "Insta Logo",
-  },
-  {
     src: "/img/Linkedin.png",
-    link: "www.google.com",
+    link: "https://www.linkedin.com/in/milind-ravikant-patil-a2429327b/",
     alt: "Linkedin Logo",
   },
   {
     src: "/img/Github.png",
-    link: "www.google.com",
+    link: "https://github.com/milind100",
     alt: "Github Logo",
   },
   {
     src: "/img/Twitter.png",
     link: "www.google.com",
     alt: "Twitter Logo",
+  },
+  {
+    src: "/img/Facebook.png",
+    link: "https://www.facebook.com/VikramadityaSpeaking/",
+    alt: "Facebook Logo",
+  },
+  {
+    src: "/img/Insta.png",
+    link: "https://www.instagram.com/va_speaking/",
+    alt: "Insta Logo",
   },
 ];
